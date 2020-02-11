bce2bee3a94787e0a8c7e2811de4688eff286238
maxk <maxk@corp.doctor.com>
2020-02-11 00:05:02 -0800

    cooler solar system

--- scripts/solarSystem.js
+++ scripts/solarSystem.js
@@ -127,11 +127,50 @@ function solarSystem() {
     earthOrbitNode.localMatrix = m4.translation(175, 0, 0);  // earth orbit 100 units from the sun
     var moonOrbitNode = new Node();
     moonOrbitNode.localMatrix = m4.translation(30, 0, 0);  // moon 30 units from the earth
+    
     var cameraNode = new Node();
-    cameraNode.localMatrix = m4.translation(250, 50, 300);
+    cameraNode.localMatrix = m4.translation(250, 0, 300);
+    var cameraMoonOrbit = new Node();
+    cameraMoonOrbit.localMatrix = m4.translation(45, 0, 0);
+    
+    var cameraMoon1Node = new Node();
+    cameraMoon1Node.localMatrix = m4.translation(75, 0, 20);
+    cameraMoon1Node.localMatrix = m4.scaling(0.4, 0.4, 0.4);
+    cameraMoon1Node.drawInfo = {
+        uniforms: {
+            u_colorOffset: [0.6, 0.6, 0.6, 1],  // gray
+            u_colorMult:   [0.1, 0.1, 0.1, 1],
+        },
+        programInfo: programInfo,
+        bufferInfo: sphereBufferInfo,
+    };
+    
+    var cameraMoon2Node = new Node();
+    cameraMoon2Node.localMatrix = m4.translation(5, 10, 0);
+    cameraMoon2Node.localMatrix = m4.scaling(0.4, 0.4, 0.4);
+    cameraMoon2Node.drawInfo = {
+        uniforms: {
+            u_colorOffset: [0.6, 0.6, 0.6, 1],  // gray
+            u_colorMult:   [0.1, 0.1, 0.1, 1],
+        },
+        programInfo: programInfo,
+        bufferInfo: sphereBufferInfo,
+    };
+    
+    var cameraMoon3Node = new Node();
+    cameraMoon3Node.localMatrix = m4.translation(0, 20, 10);
+    cameraMoon3Node.localMatrix = m4.scaling(0.4, 0.4, 0.4);
+    cameraMoon3Node.drawInfo = {
+        uniforms: {
+            u_colorOffset: [0.6, 0.6, 0.6, 1],  // gray
+            u_colorMult:   [0.1, 0.1, 0.1, 1],
+        },
+        programInfo: programInfo,
+        bufferInfo: sphereBufferInfo,
+    };
     
     var cameraNodeVisual = new Node();
-    cameraNodeVisual.localMatrix = m4.scaling(4, 4, 4);   // make the earth twice as large
+    cameraNodeVisual.localMatrix = m4.scaling(3, 3, 3);   // make the earth twice as large
     cameraNodeVisual.drawInfo = {
         uniforms: {
             u_colorOffset: [0.9, 0.2, 0, 1],  // blue-green
@@ -181,19 +220,30 @@ function solarSystem() {
     earthNode.setParent(earthOrbitNode);
     moonOrbitNode.setParent(earthOrbitNode);
     moonNode.setParent(moonOrbitNode);
-    cameraNode.setParent(earthOrbitNode);
+    
+    cameraNode.setParent(solarSystemNode);
     cameraNodeVisual.setParent(cameraNode);
+    cameraMoonOrbit.setParent(cameraNode);
+    cameraMoon1Node.setParent(cameraMoonOrbit);
+    cameraMoon2Node.setParent(cameraMoonOrbit);
+    cameraMoon3Node.setParent(cameraMoonOrbit);
 
     var objects = [
         sunNode,
         earthNode,
         moonNode,
+        cameraMoon1Node,
+        cameraMoon2Node,
+        cameraMoon3Node
     ];
 
     var objectsToDraw = [
         sunNode.drawInfo,
         earthNode.drawInfo,
         moonNode.drawInfo,
+        cameraMoon1Node.drawInfo,
+        cameraMoon2Node.drawInfo,
+        cameraMoon3Node.drawInfo
     ];
     
     var outsideCam = true;
@@ -227,12 +277,15 @@ function solarSystem() {
             m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
         
         // update the local matrices for each object.
+        m4.multiply(m4.yRotation(0.006), cameraNode.localMatrix, cameraNode.localMatrix);
         m4.multiply(m4.yRotation(0.01), earthOrbitNode.localMatrix, earthOrbitNode.localMatrix);
         m4.multiply(m4.yRotation(0.01), moonOrbitNode.localMatrix, moonOrbitNode.localMatrix);
         // spin the earth
         m4.multiply(m4.yRotation(0.05), earthNode.localMatrix, earthNode.localMatrix);
         // spin the moon
         m4.multiply(m4.yRotation(-0.01), moonNode.localMatrix, moonNode.localMatrix);
+        m4.multiply(m4.yRotation(0.007), cameraNodeVisual.localMatrix, cameraNodeVisual.localMatrix);
+        m4.multiply(m4.yRotation(0.005), cameraMoonOrbit.localMatrix, cameraMoonOrbit.localMatrix);
 
         // Update all world matrices in the scene graph
         solarSystemNode.updateWorldMatrix();
@@ -242,7 +295,7 @@ function solarSystem() {
         var cameraPosition;
         if(outsideCam){
         //cameraPosition = [0, Math.cos(time * .1), 0, Math.sin(time * .1)];
-            cameraPosition = [Math.cos(time * .1) * 700, 100, Math.sin(time * .1) * 700];
+            cameraPosition = [Math.cos(time * .1) * 700, Math.cos(time * .1) * 200, Math.sin(time * .1) * 700];
            //cameraPosition = [0, -300, 700];
         }else{
             cameraPosition = cameraNode.worldMatrix;
